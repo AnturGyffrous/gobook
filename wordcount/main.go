d19befecc6d050f462e82e93adc127d385af8dce
Owain Williams <owain@hedfan.uk>
2024-07-11 00:24:17 +0100

    Use strings.Fields to count the words, not spaces

--- wordcount/main.go
+++ wordcount/main.go
@@ -1,19 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// Use hard-coded text to start.
 	text := " let's count  some words! "
-
-	var numSpaces int
-
-	// Look at the text one byte at a time.
-	for i := 0; i < len(text); i++ {
-		if text[i] == ' ' {
-			numSpaces++
-		}
-	}
-
-	fmt.Println("Found", numSpaces+1, "words")
+	words := strings.Fields(text)
+	fmt.Println("Found", len(words), "words")
 }
