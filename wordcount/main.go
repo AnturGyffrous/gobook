acab7a1df113982635e9b697582aa8df8e535421
Owain Williams <owain@hedfan.uk>
2024-07-11 15:48:36 +0100

    Read multiple files from the command line

--- wordcount/main.go
+++ wordcount/main.go
@@ -13,31 +13,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Open the first argument as a file.
-	file, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+	// Loop over all the arguments from index 1 onwards.
+	for _, filename := range os.Args[1:] {
+		file, err := os.Open(filename)
+
+		// If there's an error, print it and skip to the next file.
+		if err != nil {
+			log.Printf("%s: %v", filename, err)
+			continue
+		}
+
+		// Create a scanner to read from the file.
+		scanner := bufio.NewScanner(file)
+
+		// Change the split function to split on words instead of lines.
+		scanner.Split(bufio.ScanWords)
+
+		// Counter to track the running total.
+		var wordCount int
+
+		// Add 1 to the count for each word
+		for scanner.Scan() {
+			wordCount++
+		}
+
+		if scanner.Err() != nil {
+			log.Printf("scan error %s: %v", filename, scanner.Err())
+			file.Close()
+			continue
+		}
+
+		file.Close()
+		fmt.Printf("%s: %d\n", filename, wordCount)
 	}
-
-	// Create a scanner to read fri the file.
-	scanner := bufio.NewScanner(file)
-
-	// Change the split function to split on words instead of lines.
-	scanner.Split(bufio.ScanWords)
-
-	// Counter to track the running total.
-	var wordCount int
-
-	// Add 1 to the count for each word
-	for scanner.Scan() {
-		wordCount++
-	}
-
-	if scanner.Err() != nil {
-		log.Println(scanner.Err())
-		os.Exit(1)
-	}
-
-	fmt.Println("Found", wordCount, "words")
 }
